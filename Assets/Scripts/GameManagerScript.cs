df4bd2fd1125e10c572372a5e7c4c4a9cb92aea7
LeadingIsmi <83781886+LeadingIsmi@users.noreply.github.com>
2023-12-19 14:43:10 +0300

    release

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,3 +1,413 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fa734a82b52fc5ab5c0d1b9561617330c1d3bd15932a075077e4bb8afe213f55
-size 11775
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Game
+{
+    public HeroManagerScript Player, Enemy;
+    public List<Card> PlayerDeck, EnemyDeck;
+    
+    public Game()
+    {
+        PlayerDeck = GiveDeckCard();
+        EnemyDeck = GiveDeckCard();
+
+        Player = new HeroManagerScript();
+        Enemy = new HeroManagerScript();
+    }
+
+    List<Card> GiveDeckCard()
+    {
+        List<Card> list = new List<Card>();
+
+        list.Add(CardManager.AllCards[10].GetCopy());
+
+        for (int i = 0; i < 30; i++)
+        {
+            var card = CardManager.AllCards[Random.Range(0, CardManager.AllCards.Count)];
+
+            if(card.IsSpell)
+                list.Add(((SpellCard)card).GetCopy());
+            else
+                list.Add(card.GetCopy()); 
+        }
+            
+        return list;
+    }
+}
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static GameManagerScript Instance;
+
+    public Game CurrentGame;
+    public Transform PlayerHand, PlayerField, EnemyHand, EnemyField;
+    public GameObject CardPref;
+    public int Turn, ReadyTime;
+    public float TurnTime;
+    public HeroAttackedScript EnemyHero, PlayerHero;
+    public AIControllerScript EnemyAI;
+
+    public List<CardControllerScript> PlayerHandCards = new List<CardControllerScript>(),
+                                      EnemyHandCards = new List<CardControllerScript>(),
+                                      PlayerFieldCards = new List<CardControllerScript>(),
+                                      EnemyFieldCards = new List<CardControllerScript>();                                    
+
+    public bool IsPlayerTurn
+    {
+        get
+        {
+            return Turn % 2 == 0;
+        }
+    }
+
+    public void Awake()
+    {
+        if(Instance == null)
+            Instance = this;
+    }
+
+    void Start()
+    {
+        StartWindow();
+    }
+
+    public void StartWindow()
+    {
+        UIControllerScript.Instance.ShowStartWindow();
+        AudioControllerScript.Instance.PlayStartWindowAudio();
+        CleanGame();
+    }
+
+    public void RestartGame()
+    {
+        AudioControllerScript.Instance.PlaySettings();
+        CleanGame();
+        StartGame();
+    }
+
+    public void MenuGame()
+    {
+        AudioControllerScript.Instance.PlaySettings();
+        UIControllerScript.Instance.ShowMenu();
+    }
+
+    public void ContinueGame()
+    {
+        AudioControllerScript.Instance.PlaySettings();
+        UIControllerScript.Instance.ShowStartGame();
+        CheckResult();
+    }
+
+    public void QuitGame()
+    {
+        AudioControllerScript.Instance.PlaySettings();
+        CleanGame();
+        Process.GetCurrentProcess().Kill();
+        //Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        CurrentGame = new Game();
+        ReadyTime = 20;
+
+        Turn = 1;
+        TurnTime = 30;
+
+        GiveHandCards(CurrentGame.PlayerDeck, PlayerHand);
+        GiveHandCards(CurrentGame.EnemyDeck, EnemyHand);
+
+        AudioControllerScript.Instance.PlayBGAudio();
+        UIControllerScript.Instance.UpdateHeroHPAndMana();
+        UIControllerScript.Instance.UpdateTurnAndCountCards();
+        UIControllerScript.Instance.UpdateTurnTime(TurnTime);
+        UIControllerScript.Instance.UpdateWickTime(TurnTime);
+        UIControllerScript.Instance.ShowFirstCard();
+
+        StartCoroutine(GiveFirstCardsFunc());
+    }
+
+    public void StartTurn()
+    {
+        StopCoroutine(GiveFirstCardsFunc());
+
+        AudioControllerScript.Instance.PlayChangeTurn();
+        UIControllerScript.Instance.ShowStartGame();
+        StartCoroutine(TurnFunc());
+    }
+
+    void GiveHandCards(List<Card> deck, Transform hand)
+    {
+        for (int i = 0; i < 4; i++)
+            GiveCardToHand(deck, hand);
+    }
+
+    void GiveCardToHand(List<Card> deck, Transform hand)
+    {
+        if (deck.Count == 0)
+            return;
+
+        CreateCardPref(deck[0], hand);
+        deck.RemoveAt(0);
+    }
+
+    public void ReplaceStartingCards()
+    {
+        AudioControllerScript.Instance.PlaySettings();
+
+        List<Card> replacedCards = new List<Card>();
+
+        foreach (var card in PlayerHandCards)
+        {
+            replacedCards.Add(card.Card);
+            Destroy(card.gameObject);
+        }
+
+        PlayerHandCards.Clear();
+
+        CurrentGame.PlayerDeck.AddRange(replacedCards);
+        GiveHandCards(CurrentGame.PlayerDeck, PlayerHand);
+        UIControllerScript.Instance.ReplaceBtn.interactable = false;
+    }
+
+    void CreateCardPref(Card card, Transform hand)
+    {
+        GameObject cardGO = Instantiate(CardPref, hand, false);
+        CardControllerScript cardC = cardGO.GetComponent<CardControllerScript>();
+
+        cardC.Init(card, hand == PlayerHand);
+
+        if (cardC.IsPlayerCard)
+            PlayerHandCards.Add(cardC);
+        else
+            EnemyHandCards.Add(cardC);
+    }
+
+    IEnumerator GiveFirstCardsFunc()
+    {    
+        UIControllerScript.Instance.UpdateReadyTime(ReadyTime);
+
+        while (ReadyTime-- > 0)
+        {
+
+            UIControllerScript.Instance.UpdateReadyTime(ReadyTime);
+            yield return new WaitForSeconds(1);
+        }
+
+        StartTurn();
+    }
+
+    IEnumerator TurnFunc()
+    {
+        UIControllerScript.Instance.UpdateTurnTime(TurnTime);
+        UIControllerScript.Instance.UpdateWickTime(TurnTime);
+
+        foreach (var card in PlayerFieldCards)
+            card.Info.AttackLightCard(false);
+
+        CheckCardsForMana();
+
+        if (IsPlayerTurn)
+        {
+            UIControllerScript.Instance.ShowStartTurn();
+            yield return new WaitForSeconds(1.5f);
+            UIControllerScript.Instance.CloseStartTurn();
+
+            foreach (var card in PlayerFieldCards)
+            {
+                if (card.Card.Atc > 0)
+                    card.Card.CanAttack = true;
+                card.Info.AttackLightCard(true);
+                card.Ability.OnNewTurn();
+            }
+
+            while (TurnTime-- > 0)
+            {
+                UIControllerScript.Instance.UpdateWickTime(TurnTime);
+                UIControllerScript.Instance.UpdateTurnTime(TurnTime);
+                yield return new WaitForSeconds(1);
+            }
+
+            ChangeTurn();
+        }
+        else
+        {
+            UIControllerScript.Instance.ShowStartTurn();
+            yield return new WaitForSeconds(1.5f);
+            UIControllerScript.Instance.CloseStartTurn();
+
+            foreach (var card in EnemyFieldCards)
+            {
+                if (card.Card.Atc > 0)
+                    card.Card.CanAttack = true;
+                card.Ability.OnNewTurn();
+            }
+
+            EnemyAI.MakeTurn();
+
+            while (TurnTime-- > 0)
+            {
+                UIControllerScript.Instance.UpdateWickTime(TurnTime);
+                UIControllerScript.Instance.UpdateTurnTime(TurnTime);
+                yield return new WaitForSeconds(1);
+            }
+
+            ChangeTurn();
+        }       
+    }
+
+    public void ChangeTurn()
+    {
+        StopAllCoroutines();
+
+        TurnTime = 30;
+        Turn++;
+
+        UIControllerScript.Instance.DisableTurnBtn();
+        AudioControllerScript.Instance.PlayChangeTurn();
+
+        if (IsPlayerTurn)
+        {
+            if (PlayerHandCards.Count < 10)
+                GiveCardToHand(CurrentGame.PlayerDeck, PlayerHand);
+
+            CurrentGame.Player.IncreaseManaPool();
+            CurrentGame.Player.RestoreRoundMana();
+
+            UIControllerScript.Instance.UpdateHeroHPAndMana();
+        }
+        else
+        {
+            if(EnemyHandCards.Count < 10)
+            {
+                for (int i = 0; i < Random.Range(1, 3); i++)
+                    GiveCardToHand(CurrentGame.EnemyDeck, EnemyHand);
+            }
+
+            CurrentGame.Enemy.IncreaseManaPool();
+            CurrentGame.Enemy.RestoreRoundMana();
+
+            UIControllerScript.Instance.UpdateHeroHPAndMana();
+        }
+
+        UIControllerScript.Instance.UpdateTurnAndCountCards();
+
+        StartCoroutine(TurnFunc());
+    }
+
+    public void FightCards(CardControllerScript attacker, CardControllerScript defender)    
+    {
+        defender.Card.GetDamage(attacker.Card.Atc);
+        attacker.OnDamageDeal();
+        defender.OnTakeDamage(attacker);
+
+        attacker.Card.GetDamage(defender.Card.Atc);
+        defender.OnDamageDeal();
+        attacker.OnTakeDamage(defender);
+
+        attacker.CheckForAlive();
+        defender.CheckForAlive();
+    }
+
+    public void ReduceMana(bool playerMana, int manacost)
+    {
+        if (playerMana)
+            CurrentGame.Player.Mana -= manacost;
+        else
+            CurrentGame.Enemy.Mana -= manacost;
+
+        UIControllerScript.Instance.UpdateHeroHPAndMana();
+    }
+
+    public void DamageHero(CardControllerScript card, bool isEnemyAttacked)
+    {
+        if (isEnemyAttacked)
+            CurrentGame.Enemy.GetDamage(card.Card.Atc);
+        else
+            CurrentGame.Player.GetDamage(card.Card.Atc);
+
+        UIControllerScript.Instance.UpdateHeroHPAndMana();
+        card.OnDamageDeal();
+        CheckResult();
+    }
+
+    public void CheckResult()
+    {
+        if (CurrentGame.Enemy.HP == 0 || CurrentGame.Player.HP == 0)
+        {
+            StopCoroutine(EnemyAI.EnemyTurn(EnemyHandCards));
+            StopCoroutine(EnemyAI.EnemyTurn(EnemyFieldCards));
+            StopAllCoroutines();
+            UIControllerScript.Instance.ShowResult();
+        } 
+    }
+
+    public void CheckCardsForMana()
+    {
+        foreach (var card in PlayerHandCards)
+            card.Info.ManaLightCard(CurrentGame.Player.Mana);
+    }
+
+    public void ShowTargetsLight(CardControllerScript attacker, bool highlight)
+    {
+        List<CardControllerScript> targets = new List<CardControllerScript>();
+
+        if (attacker.Card.IsSpell)
+        {
+            var spellCard = (SpellCard)attacker.Card;
+
+            if (spellCard.SpellTarget == SpellCard.SpellTargetType.NO_TARGET)
+                targets = new List<CardControllerScript>();
+            else if (spellCard.SpellTarget == SpellCard.SpellTargetType.PLAYER_CARD_TARGET)
+                targets = PlayerFieldCards;
+            else if (spellCard.SpellTarget == SpellCard.SpellTargetType.ENEMY_CARD_TARGET)
+                targets = EnemyFieldCards;
+        }
+        else
+        {
+            if (EnemyFieldCards.Exists(x => x.Card.IsProvocation))
+                targets = EnemyFieldCards.FindAll(x => x.Card.IsProvocation);
+            else
+            {
+                targets = EnemyFieldCards;
+                EnemyHero.TargetLightCard(highlight);
+            }
+        }
+
+        foreach (var card in targets)
+        {
+            if(attacker.Card.IsSpell)
+                card.Info.SpellTargetLightCard(highlight);
+            else
+                card.Info.TargetLightCard(highlight);
+        }
+    }
+
+    public void CleanGame()
+    {
+        StopAllCoroutines();
+        StopCoroutine(EnemyAI.EnemyTurn(EnemyHandCards));
+        StopCoroutine(EnemyAI.EnemyTurn(EnemyFieldCards));
+
+        foreach (var card in PlayerHandCards)
+            Destroy(card.gameObject);
+        foreach (var card in PlayerFieldCards)
+            Destroy(card.gameObject);
+        foreach (var card in EnemyHandCards)
+            Destroy(card.gameObject);
+        foreach (var card in EnemyFieldCards)
+            Destroy(card.gameObject);
+
+        PlayerHandCards.Clear();
+        PlayerFieldCards.Clear();
+        EnemyHandCards.Clear();
+        EnemyFieldCards.Clear();       
+    }
+
+}
+
+
--- Assets/Scripts/UIControllerScript.cs
+++ Assets/Scripts/UIControllerScript.cs
@@ -1,3 +1,154 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:28651dd67476443da5d7d35cb26a4728d2a8f085de5c818100e66642b149cab1
-size 4074
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControllerScript : MonoBehaviour
+{
+    public static UIControllerScript Instance;
+
+    public GameObject StartWindow;
+    public GameObject BackGround;
+
+    public GameObject PlayerFirstHand;
+    public TextMeshProUGUI ReadyTime;
+    public Button ReadyBtn;
+    public Button ReplaceBtn;
+
+    public TextMeshProUGUI PlayerMana, EnemyMana;
+    public TextMeshProUGUI PlayerHP, EnemyHP;
+
+    public TextMeshProUGUI PlayerCountCards, EnemyCountCards;
+    public TextMeshProUGUI Turn;  
+
+    public TextMeshProUGUI TurnTime;
+    public Button MenuBtn;
+    public Button EndTurnBtn;
+
+    public GameObject MenuObj;
+    public Scrollbar VolumeScrollbar;
+
+    public GameObject StartTurnObj;
+    public TextMeshProUGUI StartTurnTxt;
+
+    public GameObject ResultObj;
+    public TextMeshProUGUI ResultTxt;
+
+    public Image Wick;
+
+    private void Awake()
+    {
+        if (!Instance)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    public void ShowStartWindow()
+    {
+        StartWindow.SetActive(true);
+        ResultObj.SetActive(false);
+        StartTurnObj.SetActive(false);
+        MenuObj.SetActive(false);
+        BackGround.SetActive(false);
+    }
+
+    public void ShowFirstCard()
+    {
+        //foreach (var card in GameManagerScript.Instance.PlayerHandCards)
+        //card.Info.GetComponent<CanvasGroup>().alpha = 0;
+
+        ReplaceBtn.interactable = true;
+        BackGround.SetActive(true);
+        StartWindow.SetActive(false);
+        MenuObj.SetActive(false);
+        ResultObj.SetActive(false);
+        StartTurnObj.SetActive(false);
+        PlayerFirstHand.SetActive(true);      
+    }
+
+    public void ShowStartGame()
+    {
+        EndTurnBtn.interactable = GameManagerScript.Instance.IsPlayerTurn;
+
+        PlayerFirstHand.SetActive(false);
+        ResultObj.SetActive(false);
+        MenuObj.SetActive(false);
+        StartTurnObj.SetActive(false);
+
+        UpdateHeroHPAndMana();
+        UpdateTurnAndCountCards();
+    }
+
+    public void ShowMenu()
+    {
+        MenuObj.SetActive(true);     
+    }
+
+    public void ShowStartTurn()
+    {
+        StartTurnObj.SetActive(true);
+
+        if (GameManagerScript.Instance.IsPlayerTurn)
+            StartTurnTxt.text = "Your turn";
+        else
+            StartTurnTxt.text = "Enemy turn";
+    }
+
+    public void CloseStartTurn()
+    {
+        StartTurnObj.SetActive(false);
+    }
+
+    public void ShowResult()
+    {
+        StopAllCoroutines();
+        ResultObj.SetActive(true);
+
+        if (GameManagerScript.Instance.CurrentGame.Player.HP == 0)
+            ResultTxt.text = "You lose :(";
+        else
+            ResultTxt.text = "GG WP!";
+    }
+
+    public void UpdateTurnAndCountCards()
+    {
+        PlayerCountCards.text = GameManagerScript.Instance.CurrentGame.PlayerDeck.Count.ToString();
+        EnemyCountCards.text = GameManagerScript.Instance.CurrentGame.EnemyDeck.Count.ToString();
+
+        Turn.text = GameManagerScript.Instance.Turn.ToString();
+    }
+
+    public void UpdateHeroHPAndMana()
+    {
+        PlayerMana.text = GameManagerScript.Instance.CurrentGame.Player.Mana.ToString();
+        EnemyMana.text = GameManagerScript.Instance.CurrentGame.Enemy.Mana.ToString();
+
+        PlayerHP.text = GameManagerScript.Instance.CurrentGame.Player.HP.ToString();
+        EnemyHP.text = GameManagerScript.Instance.CurrentGame.Enemy.HP.ToString();
+    }
+
+    public void UpdateWickTime(float time)
+    {
+        Wick.fillAmount = time / 30f;
+    }
+
+    public void UpdateTurnTime(float time)
+    {
+        TurnTime.text = time.ToString();
+    }
+
+    public void UpdateReadyTime(int time)
+    {
+        ReadyTime.text = time.ToString();
+    }
+
+    public void DisableTurnBtn()
+    {
+        EndTurnBtn.interactable = GameManagerScript.Instance.IsPlayerTurn;
+    }
+}
